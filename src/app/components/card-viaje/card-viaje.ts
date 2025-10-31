baa733037df711a6619a39a27dd340e8708bb3b7
Javier Ortiz <jrortiz289@gmail.com>
2025-11-01 00:10:41 +0100

    cambios en maquetación e implementación del boton favorito para futuro uso.

--- src/app/components/card-viaje/card-viaje.ts
+++ src/app/components/card-viaje/card-viaje.ts
@@ -8,6 +8,48 @@ import { Component } from '@angular/core';
 })
 export class CardViaje {
 
+  getShowDaysBadge(startDate: string): boolean {
+  const today = new Date();
+  const start = new Date(startDate);
+  const diff = Math.ceil((start.getTime() - today.getTime()) / (1000 * 60 * 60 * 24));
+  return diff < 15 && diff > 0;
+}
+
+getDaysLeft(startDate: string): number {
+  const today = new Date();
+  const start = new Date(startDate);
+  return Math.ceil((start.getTime() - today.getTime()) / (1000 * 60 * 60 * 24));
+}
+
+getBadgeClass(status: string): string {
+  switch (status) {
+    case 'open': return 'bg-success';
+    case 'draft': return 'bg-danger';
+    default: return 'bg-secondary';
+  }
+}
+
+getBadgeText(status: string): string {
+  switch (status) {
+    case 'open': return 'Abierto';
+    case 'draft': return 'Últimos días';
+    default: return status;
+  }
+}
+
+// toggleFavorito(trip: Trip) {
+//   // Cambia el estado local
+//   trip.isFavorite = !trip.isFavorite;
+
+//   // Aquí puedes añadir la petición a la API si quieres guardar favorito en el servidor:
+//   // this.miServicioViajes.guardarFavorito(trip.id, trip.isFavorite).subscribe(...);
+// }
+
+getFavoriteIconClass(isFavorite: boolean): string {
+  return isFavorite ? 'bi-heart-fill text-danger' : 'bi-heart text-white';
+}
+
+
 trips = [
   {
     id: 1,
@@ -17,8 +59,8 @@ trips = [
     image: "https://content-viajes.nationalgeographic.com.es/medio/2024/11/07/alfama_151256d3_241107153719_1200x800.webp",
     description: "Un viaje de 5 días recorriendo la costa ibérica.",
     creator_id: 1,
-    start_date: "2025-07-10",
-    end_date: "2025-07-15",
+    start_date: "2025-11-11",
+    end_date: "2025-11-25",
     estimated_cost: 450.00,
     min_participants: 3,
     transport: "Coche",
@@ -66,12 +108,13 @@ trips = [
     transport: "Furgoneta",
     accommodation: "Camping",
     itinerary: "Día 1: Encuentro en Bilbao...",
-    status: "draft",
+    status: "open",
     latitude: 43.25510000,
     longitude: -4.63330000,
     created_at: "2025-10-27 08:39:38",
     updated_at: "2025-10-27 08:39:38"
-  }
+  },
+  
 ]
   
 }
