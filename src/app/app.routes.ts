a674743867c186dfc2443a6757a026cdb079af59
Leanygonzalez <leany.gonzalez@hotmail.com>
2025-11-01 14:05:39 +0100

    feat(header): añade logo y branding de la aplicación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { searchearchComponent } from './pages/search/search';
+import { LoginComponent } from './pages/login/login';
+import { RegisterComponent } from './pages/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: SearchComponent },
+  { path: 'buscar', component: SearchComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegisterComponent },
+  { path: '**', redirectTo: 'buscar' },
+];
