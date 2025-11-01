b89f2f294d5b0db4012e79d7d2a5f089ab98eca2
Leany.Gonzalez <leany.gonzalez@hotmail.com>
2025-11-01 14:23:56 +0100

    corrección app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { SearchComponent } from './pages/search/search';
+import { LoginComponent } from './pages/loging/loging';
+import { RegisterComponent } from './pages/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: SearchComponent },
+  { path: ' buscar', component: SearchComponent },
+  { path: 'login', component: LogingComponent },
+  { path: 'registro', component: registroComponent },
+  { path: '**', redirectTo: 'buscar' },
+];
